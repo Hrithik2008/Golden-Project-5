fcaf5ea54c3bea9294f8e470236492ffe32dba1d
Hrithik <sudhakar7805@gmail.com>
2021-05-07 17:03:46 +0530

    Added movement and bullets

--- js/Game.js
+++ js/Game.js
@@ -18,42 +18,51 @@ class Game {
     form.display();
     player.getCount();
     for(var i = 0; i <= 4;i++){
-        objects.push(new PlayerObject(200,200,50,50));
+        objects.push(new PlayerObject(600,400,50,50));
     }
   }
   play() {
+    background(bg);
     Player.getPlayerInfo();
     var position = displayHeight / 2;
     form.hideForm();
     for(var i = 1; i < objects.length+1;i++){
         let index = "Player"+i;
-        objects[i-1].x = allPlayers[index].x;
-        objects[i-1].y = allPlayers[index].y;
+        Matter.Body.setPosition(objects[i-1].body,{x : allPlayers[index].x,y: allPlayers[index].y});
 
         objects[i-1].display();
     }
+    for(var k in bullets){
+      bullets[k].display();
+    }
     if (allPlayers !== undefined) {
       for (var i in allPlayers) {
+        position = {x : allPlayers[i].x - 20, y : allPlayers[i].y - 25};
         if (i === "Player" + player.index) {
           player.x = allPlayers[i].x;
           player.y = allPlayers[i].y;
           player.health = allPlayers[i].health;
           player.rank = allPlayers[i].rank;
           fill(0, 255, 0);
-          textSize(18);
+          ellipse(allPlayers[i].x,allPlayers[i].y,50,50)
+          //Body.rotate(objects[player.index-1].body,{x : mouseX , y : mouseY})
         } else {
-          fill(0);
+          fill(255);
         }
-        //text(`${allPlayers[i].name}|| position = {${allPlayers[i].x},${allPlayers[i].y}} || health = ${allPlayers[i].health} || rank = ${allPlayers[i].rank}`,120,position);
-        position += 50;
+        textSize(18);
+        text(`${allPlayers[i].name}`,position.x,position.y);
       }
     }
+    if(keyIsDown(32)){
+      var bullet = new Bullet(player.x,player.y,25);
+      Body.applyForce(bullet.body,{x : player.x , y : player.y},2000);
+      bullets.push(bullet);
+    }
     if (keyIsDown(87)) {
-      player.y += 10;
-      console.log(player.y);
+      player.y -= 10;
       player.update();
     } else if (keyIsDown(83)) {
-      player.y -= 10;
+      player.y += 10;
       player.update();
     }
     if (keyIsDown(65)) {
--- sketch.js
+++ sketch.js
@@ -4,24 +4,32 @@ const Bodies = Matter.Bodies;
 const Body = Matter.Body;
 const Constraint = Matter.Constraint;
 var database;
-var playerCount,gameState=0,player,game,form,allPlayers,objects=[];
+var playerCount,gameState=0,player,game,form,allPlayers,objects=[],bullets = [];
+var bg;
+
+function preload(){
+  bg = loadImage("images/bg.jpg");
+}
 
 function setup() {
   createCanvas(windowWidth,windowHeight);
   engine = Engine.create();
   world = engine.world; 
   database = firebase.database();
-  
+  //world.gravity.y = 0;
+
   game = new Game();
 
   game.start();
   game.getState();
 
+  //Bodies.rectangle(width/2,height,width,25,{isStatic : true});
+
 }
 
 function draw() {
-  //Engine.update(engine);
-  background(255);
+  Engine.update(engine);
+  //background(255);
   if(playerCount > 4 && keyIsDown(UP_ARROW)){
     game.updateState(1);
   }
